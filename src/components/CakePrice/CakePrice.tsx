c5636008c6ad3dda1e4ab1e71cd93de67b9a53c4
arifsajal70 <arifulislamsajal1109@gmail.com>
2021-11-22 06:18:16 +0600

    Initial commit

---
+++ src/components/CakePrice/CakePrice.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { ReaconRoundIcon } from "../Svg";
+import Text from "../Text/Text";
+import Skeleton from "../Skeleton/Skeleton";
+import { Colors } from "../../theme";
+
+export interface Props {
+  color?: keyof Colors;
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
+  return cakePriceUsd ? (
+    <PriceLink
+      href="/swap?outputCurrency=0x50134f050C420Db356CA2b45F3EE6E601a7D6475"
+      target="_blank"
+    >
+      <ReaconRoundIcon width="24px" mr="8px" />
+      <Text color={color} bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
